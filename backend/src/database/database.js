323c15fa487cbb2f2d8b59bc19cedcb48bdd5c0d
Azula <Azula@gmail.com>
2025-11-13 17:39:01 +0530

    [FIX] Validation chek for database and fixed previous logic

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -3,11 +3,15 @@ import ENV from "../lib/env.js";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(ENV?.DB_URL);
+    if (ENV.DB_URL) {
+      throw new Error("DB_URL is not defined in the ENV");
+    }
+
+    const conn = await mongoose.connect(ENV.DB_URL);
     console.log("Database connected --->", conn.connection.host);
   } catch (error) {
     console.log("Error connecting to Database", error);
-    process.exit(1);
+    process?.exit(1);
   }
 };
 
