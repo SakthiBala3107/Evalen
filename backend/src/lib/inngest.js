011a5e534059393de223af4ce1578bfa2291ad14
Azula <Azula@gmail.com>
2025-11-14 13:40:23 +0530

    [FIX]sl=ubtle chnages in env  files

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,47 +1,39 @@
 import { Inngest } from "inngest";
+
 import connectDB from "../database/database.js";
 import User from "../models/User.model.js";
+import ENV from "./env.js";
 
-export const inngest = new Inngest({ id: "evalen" });
+export const inngest = new Inngest({
+  id: "evalen",
+  signingKey: ENV.INNGEST_SIGNING_KEY,
+  eventKey: ENV.INNGEST_EVENT_KEY,
+});
 
+// rest of your functions
 const syncUser = inngest.createFunction(
-  //   stuffs from inngest
   { id: "sync-user" },
   { event: "clerk/user.create" },
-  //   our function
   async ({ event }) => {
     await connectDB();
-
-    // user data get from the inngest
     const { id, email_addresses, first_name, last_name, image_url } =
       event.data;
-
-    //   scheme input
     const newUser = {
       clerkId: id,
       email: email_addresses[0]?.email_addresses,
       name: `${first_name || ""}${last_name || ""}`,
       profileImage: image_url,
     };
-
-    // create the new useraccording to the schema
     await User.create(newUser);
   }
 );
 
-//
 const deleteUserFromDB = inngest.createFunction(
-  //
   { id: "delete-user-from-db" },
   { event: "clerk/user.delete" },
-  //   our function
   async ({ event }) => {
     await connectDB();
-
-    //stuffs from inngest
     const { id } = event.data;
-
-    //delegte user using id (clerkIds)
     await User.deleteOne({ clerkId: id });
   }
 );
